bb2a98cfaf80dd6dc3280246252d5b259083df73
Stephen <43051770+StephenTheCoder@users.noreply.github.com>
2023-05-16 22:42:02 -0500

    Create WorkingSeqComparison.py

---
+++ WorkingSeqComparison.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from Bio.Blast.Applications import NcbiblastpCommandline
+from StringIO import StringIO
+from Bio.Blast import NCBIXML
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+# Create two sequence files
+seq1 = SeqRecord(Seq("FQTWEEFSRAAEKLYLADPMKVRVVLKYRHVDGNLCIKVTDDLVCLVYRTDQAQDVKKIEKF"),
+                   id="seq1")
+seq2 = SeqRecord(Seq("FQTWEEFSRAEKLYLADPMKVRVVLRYRHVDGNLCIKVTDDLICLVYRTDQAQDVKKIEKF"),
+                   id="seq2")
+SeqIO.write(seq1, "seq1.fasta", "fasta")
+SeqIO.write(seq2, "seq2.fasta", "fasta")
+
+# Run BLAST and parse the output as XML
+output = NcbiblastpCommandline(query="seq1.fasta", subject="seq2.fasta", outfmt=5)()[0]
+blast_result_record = NCBIXML.read(StringIO(output))
+
+# Print some information on the result
+for alignment in blast_result_record.alignments:
+    for hsp in alignment.hsps:
+        print '****Alignment****'
+        print 'sequence:', alignment.title
+        print 'length:', alignment.length
+        print 'e value:', hsp.expect
+        print hsp.query
+        print hsp.match
+        print hsp.sbjct
